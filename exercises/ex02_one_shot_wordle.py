18e44c1db01a51cfa51bf16d52926aa8d0842d3d
Natalie Ammerman <natamm@unc.edu>
2022-09-08 23:22:59 -0400

    finished x2

--- exercises/ex02_one_shot_wordle.py
+++ exercises/ex02_one_shot_wordle.py
@@ -11,6 +11,7 @@ WHITE_BOX: str = "\U00002B1C"
 GREEN_BOX: str = "\U0001F7E9"
 YELLOW_BOX: str = "\U0001F7E8"
 
+
 while len(guess) != int(len(secret)):
     guess = input("That was not int(len(secret)) letters! Try again: ")
 """Makes user reenter word if not the right amount of letters."""
