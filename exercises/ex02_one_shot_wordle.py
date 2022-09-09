1b22c648535939954a20b1bd1ecbbe32b4183598
Natalie Ammerman <natamm@unc.edu>
2022-09-08 23:53:54 -0400

    "OH MY GOD"

--- exercises/ex02_one_shot_wordle.py
+++ exercises/ex02_one_shot_wordle.py
@@ -7,6 +7,7 @@ secret: str = "python"
 guess: str = input(f"What is your {int(len(secret))}-letter guess? ")
 where: int = 0
 emoji: str = ""
+
 WHITE_BOX: str = "\U00002B1C"
 GREEN_BOX: str = "\U0001F7E9"
 YELLOW_BOX: str = "\U0001F7E8"
